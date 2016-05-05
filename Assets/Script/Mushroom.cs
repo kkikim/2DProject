cd730aa3e648fa6eaedb9db7fcbbf97a0632defd
kkikim <ddous5858@nate.com>
2016-05-05 18:45:28 +0900

    버섯생성
    박스와 충돌하면 버섯생성, 버섯과 캐릭터 충돌하면 버섯 오브젝트 삭제

---
+++ Assets/Script/Mushroom.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mushroom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        print("mushroom");
+        if(coll.gameObject.name == "Player")
+        {
+            print("mushroom2");
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Script/RandomBox.cs
+++ Assets/Script/RandomBox.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class RandomBox : MonoBehaviour {
 
+    public GameObject mushroom;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,10 +17,12 @@ public class RandomBox : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if(coll.gameObject.tag == "Player")
+        if(coll.gameObject.name == "Player")
         {
+            //버섯생성.
            print("boxEnter");
-           GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 500.0f));
+          // GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 500.0f));
+           Instantiate(mushroom,new Vector2(this.transform.position.x,this.transform.position.y+1), Quaternion.identity);
         }
     }
 
