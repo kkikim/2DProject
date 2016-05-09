716dfae20eea940335c5f5e193bd459ec984575d
kkikim <ddous5858@nate.com>
2016-05-09 20:19:06 +0900

    낙사 구현
    낙사구현

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -7,17 +7,19 @@ public class PlayerCtrl : MonoBehaviour
     float           jumpPower;              // 플레이어 캐릭터를 점프시켰을 때의 파워
     bool            isjump;
     bool            grounded;		        // 접지 확인
-
+    Animator        marioAni;
+    BoxCollider2D   marioBox;
     // Use this for initialization
     void Start()
     {
-        speed = 5;
-        jumpPower = 800.0f;
-        grounded = false;
-        isjump = false;
+        speed           = 5;
+        jumpPower       = 800.0f;
+        grounded        = false;
+        isjump          = false;
+        marioAni = GetComponent<Animator>();
+        marioBox = GetComponent<BoxCollider2D>();
     }
     // Update is called once per frame
-
     void Update()
     {
         float key = Input.GetAxis("Horizontal");
@@ -25,7 +27,7 @@ public class PlayerCtrl : MonoBehaviour
 
         //지면과의 충돌체크
         //Transform groundCheck = transform.Find("GroundCheck");
-        //grounded = (Physics2D.OverlapPoint(groundCheck.position) != null) ? true : false;
+        //grounded = (Physics2D.OverlapPoint(groundCheck.position) != null) ? true : false;z
         if(true==isjump)
         { 
             GetComponent<Animator>().SetInteger("state", 2);
@@ -49,36 +51,29 @@ public class PlayerCtrl : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //왼쪽 이동
-            //anim.SetInteger("state",1);
-            Vector3 scale = transform.localScale;
-            scale.x = -Mathf.Abs(scale.x);
-            transform.localScale = scale;
+            GetComponent<SpriteRenderer>().flipX = true;        //좌우이동 반전.
             transform.Translate(Vector3.right * amtMove * key);
             if(grounded)
+                GetComponent<Animator>().SetInteger("state", 1);
+            else if (!grounded)
+                GetComponent<Animator>().SetInteger("state", 2);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //오른쪽이동
+            GetComponent<SpriteRenderer>().flipX = false;       //좌우이동 반전.
+            transform.Translate(Vector3.right * amtMove * key);
+            if (grounded)
                 GetComponent<Animator>().SetInteger("state", 1);
             else if (!grounded)
                 GetComponent<Animator>().SetInteger("state", 2);
 
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            //오른쪽이동
-            //anim.SetInteger("state", 1);
-            Vector3 scale = transform.localScale;
-            scale.x = Mathf.Abs(scale.x);
-            transform.localScale = scale;
-            transform.Translate(Vector3.right * amtMove * key);
-            //GetComponent<Animator>().SetTrigger("walk");
-            if (grounded)
-                GetComponent<Animator>().SetInteger("state", 1);
-            else if (!grounded)
-                GetComponent<Animator>().SetInteger("state", 2);
-        }
         else
         {
-            if(grounded)
+            if (grounded)
                 GetComponent<Animator>().SetInteger("state", 0);
-            else if(!grounded)
+            else if (!grounded)
                 GetComponent<Animator>().SetInteger("state", 2);
         }
     }
@@ -90,6 +85,15 @@ public class PlayerCtrl : MonoBehaviour
             isjump = false;
             grounded = true;
             print("enter");
+        }
+        if(coll.gameObject.tag == "Fall")
+        {
+            grounded = false;
+
+            marioAni.Play("dead");
+            GetComponent<Rigidbody2D>().AddForce(transform.up*35,ForceMode2D.Impulse);
+            Destroy(marioBox);
+
         }
     }
     void OnCollisionStay2D(Collision2D coll)
@@ -103,8 +107,6 @@ public class PlayerCtrl : MonoBehaviour
     {
         if (coll.gameObject.tag == "Tile")
         {
-            //isjump = true;
-            //grounded = false;
             print("exit");
         }
     }
